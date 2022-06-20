224f3a8739e9d80329a9a49607f4cb6de268f6e3
Kasper Timm Hansen <kaspth@gmail.com>
2022-06-20 14:04:18 +0200

    Reuse ActionView::TestCase to obviate tons of setup

--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -2,13 +2,13 @@ require_relative "./test_helper"
 
 class RendererTest < NicePartials::Test
   test "render basic nice partial" do
-    rendered = @view.render("basic") { |p| p.content_for :message, "hello from nice partials" }.squish
+    render("basic") { |p| p.content_for :message, "hello from nice partials" }
 
-    assert_equal "hello from nice partials", rendered
+    assert_match "hello from nice partials", rendered
   end
 
   test "render nice partial in card template" do
-    rendered = @view.render(template: "card_test").squish
+    render(template: "card_test")
 
     assert_match "Some Title", rendered
     assert_match "Lorem Ipsum", rendered
