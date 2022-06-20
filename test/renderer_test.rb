b24d5900aca61a89061b5e0dbf8dd11e3cc17b87
Kasper Timm Hansen <kaspth@gmail.com>
2022-06-20 13:12:59 +0200

    Extract a gem common Test class, useful for splitting into other tests down the line

--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -1,44 +1,6 @@
 require_relative "./test_helper"
 
-class RendererTest < ActiveSupport::TestCase
-  # from actionview/render_test
-  class TestController < ActionController::Base
-  end
-
-  def setup_view(paths)
-    @assigns = { secret: "in the sauce" }
-
-    @view = Class.new(ActionView::Base.with_empty_template_cache) do
-      def view_cache_dependencies; []; end
-
-      def combined_fragment_cache_key(key)
-        [:views, key]
-      end
-    end.with_view_paths(paths, @assigns)
-
-    controller = TestController.new
-    controller.perform_caching = true
-    controller.cache_store = :memory_store
-    @view.controller = controller
-
-    @controller_view = controller.view_context_class.with_empty_template_cache.new(
-      controller.lookup_context,
-      controller.view_assigns,
-      controller)
-  end
-
-  setup do
-    ActionView::LookupContext::DetailsKey.clear
-    path = ActionView::FileSystemResolver.new(FIXTURE_LOAD_PATH)
-    view_paths = ActionView::PathSet.new([path])
-    assert_equal ActionView::FileSystemResolver.new(FIXTURE_LOAD_PATH), view_paths.first
-    setup_view(view_paths)
-  end
-
-  teardown do
-    ActionController::Base.view_paths.map(&:clear_cache)
-  end
-
+class RendererTest < NicePartials::Test
   test "render basic nice partial" do
     rendered = @view.render("basic") { |p| p.content_for :message, "hello from nice partials" }.squish
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,43 @@ require "action_view"
 require "nice_partials"
 
 FIXTURE_LOAD_PATH = File.expand_path("fixtures", __dir__)
+
+class NicePartials::Test < ActiveSupport::TestCase
+  # from actionview/render_test
+  class TestController < ActionController::Base
+  end
+
+  def setup_view(paths)
+    @assigns = { secret: "in the sauce" }
+
+    @view = Class.new(ActionView::Base.with_empty_template_cache) do
+      def view_cache_dependencies; []; end
+
+      def combined_fragment_cache_key(key)
+        [:views, key]
+      end
+    end.with_view_paths(paths, @assigns)
+
+    controller = TestController.new
+    controller.perform_caching = true
+    controller.cache_store = :memory_store
+    @view.controller = controller
+
+    @controller_view = controller.view_context_class.with_empty_template_cache.new(
+      controller.lookup_context,
+      controller.view_assigns,
+      controller)
+  end
+
+  setup do
+    ActionView::LookupContext::DetailsKey.clear
+    path = ActionView::FileSystemResolver.new(FIXTURE_LOAD_PATH)
+    view_paths = ActionView::PathSet.new([path])
+    assert_equal ActionView::FileSystemResolver.new(FIXTURE_LOAD_PATH), view_paths.first
+    setup_view(view_paths)
+  end
+
+  teardown do
+    ActionController::Base.view_paths.map(&:clear_cache)
+  end
+end
