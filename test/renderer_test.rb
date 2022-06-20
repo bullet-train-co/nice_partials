0f4c6552dc6bc00ec41f86c4ff4b38f6f011b278
Kasper Timm Hansen <kaspth@gmail.com>
2022-06-20 13:10:02 +0200

    on_load hook takes care of the include when require action_view is done

--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -4,10 +4,8 @@ class RendererTest < ActiveSupport::TestCase
   # from actionview/render_test
   class TestController < ActionController::Base
   end
- 
-  def setup_view(paths)
-    ActionView::Base.include(NicePartials::Helper)
 
+  def setup_view(paths)
     @assigns = { secret: "in the sauce" }
 
     @view = Class.new(ActionView::Base.with_empty_template_cache) do
