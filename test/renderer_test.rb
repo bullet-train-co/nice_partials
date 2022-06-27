1c002483a065c2bef10fa301f75de8bff9f1c8f5
Kasper Timm Hansen <kaspth@gmail.com>
2022-06-27 18:00:45 +0200

    Reuse ActionView::TestCase for testing (#25)

--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -1,57 +1,17 @@
-require_relative "./test_helper"
+require "test_helper"
 
-class RendererTest < ActiveSupport::TestCase
-  # from actionview/render_test
-  class TestController < ActionController::Base
-  end
- 
-  def setup_view(paths)
-    ActionView::Base.include(NicePartials::Helper)
-
-    @assigns = { secret: "in the sauce" }
-
-    @view = Class.new(ActionView::Base.with_empty_template_cache) do
-      def view_cache_dependencies; []; end
-
-      def combined_fragment_cache_key(key)
-        [:views, key]
-      end
-    end.with_view_paths(paths, @assigns)
-
-    controller = TestController.new
-    controller.perform_caching = true
-    controller.cache_store = :memory_store
-    @view.controller = controller
-
-    @controller_view = controller.view_context_class.with_empty_template_cache.new(
-      controller.lookup_context,
-      controller.view_assigns,
-      controller)
-  end
-
-  def setup
-    ActionView::LookupContext::DetailsKey.clear
-    path = ActionView::FileSystemResolver.new(FIXTURE_LOAD_PATH)
-    view_paths = ActionView::PathSet.new([path])
-    assert_equal ActionView::FileSystemResolver.new(FIXTURE_LOAD_PATH), view_paths.first
-    setup_view(view_paths)
-  end
-
-  def teardown
-    ActionController::Base.view_paths.map(&:clear_cache)
-  end
-
+class RendererTest < NicePartials::Test
   test "render basic nice partial" do
-    rendered = @view.render("basic") { |p| p.content_for :message, "hello from nice partials" }.squish
+    render("basic") { |p| p.content_for :message, "hello from nice partials" }
 
-    assert_equal "hello from nice partials", rendered
+    assert_rendered "hello from nice partials"
   end
 
   test "render nice partial in card template" do
-    rendered = @view.render(template: "card_test").squish
+    render(template: "card_test")
 
-    assert_match "Some Title", rendered
-    assert_match "Lorem Ipsum", rendered
-    assert_match "https://example.com/image.jpg", rendered
+    assert_rendered "Some Title"
+    assert_rendered "Lorem Ipsum"
+    assert_rendered "https://example.com/image.jpg"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ require "active_support"
 require "active_support/testing/autorun"
 require "action_controller"
 require "action_view"
+require "action_view/test_case"
+
 require "nice_partials"
 
-FIXTURE_LOAD_PATH = File.expand_path("fixtures", __dir__)
+class NicePartials::Test < ActionView::TestCase
+  TestController.view_paths << "test/fixtures"
+
+  private
+
+  def assert_rendered(matcher)
+    assert_match matcher, rendered
+  end
+end
