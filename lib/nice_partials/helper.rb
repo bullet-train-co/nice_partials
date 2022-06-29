38ba5822dc190481c981d37aed40685f78d0f953
Kasper Timm Hansen <kaspth@gmail.com>
2022-06-29 16:03:26 +0200

    Move all t related handling to the monkey patch location

--- lib/nice_partials/helper.rb
+++ lib/nice_partials/helper.rb
@@ -4,20 +4,4 @@ module NicePartials::Helper
   def np
     NicePartials::Partial.new(self)
   end
-
-  def with_nice_partials_t_prefix(lookup_context, block)
-    @_nice_partials_t_prefixes ||= []
-    @_nice_partials_t_prefixes << (block ? NicePartials.locale_prefix_from(lookup_context, block) : '')
-    yield
-  ensure
-    @_nice_partials_t_prefixes.pop
-  end
-
-  def t(key, options = {})
-    if (prefix = @_nice_partials_t_prefixes&.last) && key.first == '.'
-      key = "#{prefix}#{key}"
-    end
-
-    super(key, **options)
-  end
 end
--- lib/nice_partials/monkey_patch.rb
+++ lib/nice_partials/monkey_patch.rb
@@ -9,6 +9,24 @@ module NicePartials::RenderingWithLocalePrefix
   def capture(*, &block)
     with_nice_partials_t_prefix(lookup_context, block) { super }
   end
+
+  def t(key, options = {})
+    if (prefix = @_nice_partials_t_prefixes&.last) && key.first == '.'
+      key = "#{prefix}#{key}"
+    end
+
+    super(key, **options)
+  end
+
+  private
+
+  def with_nice_partials_t_prefix(lookup_context, block)
+    @_nice_partials_t_prefixes ||= []
+    @_nice_partials_t_prefixes << (block ? NicePartials.locale_prefix_from(lookup_context, block) : '')
+    yield
+  ensure
+    @_nice_partials_t_prefixes.pop
+  end
 end
 
 ActionView::Base.prepend NicePartials::RenderingWithLocalePrefix
