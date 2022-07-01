62c0179a003ea28f91f3e668f25cee9cb52a5b48
Kasper Timm Hansen <kaspth@gmail.com>
2022-07-01 18:56:54 +0200

    Remove unneeded `render` override (#31)
    * Remove unneeded `render` override
    
    When a partial calls `yield`, it'll automatically call into here and run
    `_layout_for`:
    https://github.com/rails/rails/blob/c704da66de59262f4e88824589ae4eddefb6ed4a/actionview/lib/action_view/renderer/partial_renderer.rb#L252
    
    If `_layout_for` is passed a block, it'll call `capture` here:
    https://github.com/rails/rails/blob/c704da66de59262f4e88824589ae4eddefb6ed4a/actionview/lib/action_view/helpers/rendering_helper.rb#L101
    
    Since we're overriding `capture`, we can omit the `render` override now.
    
    Note: `_layout_for` has a fallback to `super` in case it's passed a Symbol argument,
    e.g. `yield :body`, in which it behaves like `content_for` (as `content_for` calls `view_flow.get`).
    https://github.com/rails/rails/blob/c704da66de59262f4e88824589ae4eddefb6ed4a/actionview/lib/action_view/context.rb#L29
    
    * [ci skip] Remove stale comments; not true since capture has been overriden

--- lib/nice_partials/monkey_patch.rb
+++ lib/nice_partials/monkey_patch.rb
@@ -1,11 +1,6 @@
 # Monkey patch required to make `t` work as expected. Is this evil?
 # TODO Do we need to monkey patch other types of renderers as well?
 module NicePartials::RenderingWithLocalePrefix
-  # See `content_for` in `lib/nice_partials/partial.rb` for something similar.
-  def render(*, &block)
-    with_nice_partials_t_prefix(lookup_context, block) { super }
-  end
-
   def capture(*, &block)
     with_nice_partials_t_prefix(lookup_context, block) { super }
   end
