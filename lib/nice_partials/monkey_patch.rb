f281083e5486024d8f3a1068d9d07208271fa3bb
Kasper Timm Hansen <kaspth@gmail.com>
2022-07-03 13:37:00 +0200

    Deprecate p shorthand in favor of partial

--- lib/nice_partials/monkey_patch.rb
+++ lib/nice_partials/monkey_patch.rb
@@ -24,11 +24,19 @@ module NicePartials::RenderingWithLocalePrefix
   end
 end
 
+require "active_support/deprecation"
+NicePartials::DEPRECATOR = ActiveSupport::Deprecation.new("1.0", "nice_partials")
+
 module NicePartials::RenderingWithAutoContext
   attr_reader :partial
 
   def p(*args)
-    args.empty? ? partial : super
+    if args.empty?
+      NicePartials::DEPRECATOR.deprecation_warning :p, :partial # In-branch printing so we don't warn on legit `Kernel.p` calls.
+      partial
+    else
+      super # …we're really Kernel.p
+    end
   end
 
   def render(options = {}, locals = {}, &block)
