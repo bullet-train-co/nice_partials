3f3aafb54b1499fd119c2fb9cd9563d24d0a0cbd
Andrew Culver <andrew.culver@gmail.com>
2022-07-04 07:14:31 -0700

    Replace `p` with `partial` as the default variable? (#37)
    * Rename `content` to `partial`
    
    Fixes #36
    
    We're keeping the `_p` and `p` for now, to decide on separately.
    
    * Remove `partial` shorthand `_p` and deprecate `p` (#39)
    
    * Cut _p shorthand trial short by my own hands
    
    * Deprecate p shorthand in favor of partial
    
    Co-authored-by: Kasper Timm Hansen <kaspth@gmail.com>

--- lib/nice_partials/monkey_patch.rb
+++ lib/nice_partials/monkey_patch.rb
@@ -24,20 +24,27 @@ module NicePartials::RenderingWithLocalePrefix
   end
 end
 
+require "active_support/deprecation"
+NicePartials::DEPRECATOR = ActiveSupport::Deprecation.new("1.0", "nice_partials")
+
 module NicePartials::RenderingWithAutoContext
-  attr_reader :content
-  alias_method :_p, :content
+  attr_reader :partial
 
   def p(*args)
-    args.empty? ? _p : super
+    if args.empty?
+      NicePartials::DEPRECATOR.deprecation_warning :p, :partial # In-branch printing so we don't warn on legit `Kernel.p` calls.
+      partial
+    else
+      super # …we're really Kernel.p
+    end
   end
 
   def render(options = {}, locals = {}, &block)
-    _content, @content = content, nice_partial
-    @content.capture(block)
+    _partial, @partial = partial, nice_partial
+    @partial.capture(block)
     super
   ensure
-    @content = _content
+    @partial = _partial
   end
 end
 
