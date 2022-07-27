0f15b8b7f962fd54017e91cd4b8fdb16f22e861e
Kasper Timm Hansen <kaspth@gmail.com>
2022-07-27 03:52:44 +0200

    Allow yielding arguments to `content_for` blocks (#43)

--- lib/nice_partials/partial.rb
+++ lib/nice_partials/partial.rb
@@ -1,5 +1,7 @@
 module NicePartials
   class Partial
+    autoload :Section, "nice_partials/partial/section"
+
     delegate_missing_to :@view_context
 
     #   <%= render "nice_partial" do |p| %>
@@ -14,40 +16,41 @@ module NicePartials
 
     def initialize(view_context)
       @view_context = view_context
+      @contents = Hash.new { |h, k| h[k] = Section.new(@view_context) }
     end
 
-    def yield(name = nil)
-      raise "You can only use Nice Partial's yield method to retrieve the content of named content area blocks. If you're not trying to fetch the content of a named content area block, you don't need Nice Partials! You can just call the vanilla Rails `yield`." unless name
-      content_for(name)
+    def yield(*arguments, &block)
+      if arguments.empty?
+        raise "You can only use Nice Partial's yield method to retrieve the content of named content area blocks. If you're not trying to fetch the content of a named content area block, you don't need Nice Partials! You can just call the vanilla Rails `yield`."
+      else
+        content_for(*arguments, &block)
+      end
     end
 
     def helpers(&block)
       class_eval &block
     end
 
-    def content_for(name, content = nil, &block)
-      content = @view_context.capture(&block) if block
-
-      if content
-        contents[name] << content.to_s
-        nil
-      else
-        contents[name].presence
-      end
+    # Similar to Rails' built-in `content_for` except it defers any block execution
+    # and lets you pass arguments into it, like so:
+    #
+    #   # Here we store a block with some eventual content…
+    #   <% partial.content_for :title do |tag|
+    #     <%= tag.h1 %>
+    #   <% end %>
+    #
+    #  # …which is then invoked with some predefined options later.
+    #  <%= partial.content_for :title, tag.with_options(class: "text-bold") %>
+    def content_for(name, content = nil, *arguments, &block)
+      @contents[name].content_for(content, *arguments, &block)
     end
 
     def content_for?(name)
-      contents[name].present?
+      @contents[name].content?
     end
 
     def capture(*arguments, &block)
       self.output_buffer = @view_context.capture(*arguments, self, &block)
-    end
-
-    private
-
-    def contents
-      @contents ||= Hash.new { |h, k| h[k] = ActiveSupport::SafeBuffer.new }
     end
   end
 end
