2015f9b3e4c2a08b6683a30df83cf77ce20841bf
Kasper Timm Hansen <kaspth@gmail.com>
2022-07-31 19:36:18 +0200

    Document overrides
    This comments on our overrides giving a higher level explanation of
    what they do.

--- lib/nice_partials/monkey_patch.rb
+++ lib/nice_partials/monkey_patch.rb
@@ -46,6 +46,8 @@ module NicePartials::RenderingWithAutoContext
     end
   end
 
+  # Overrides `ActionView::Helpers::RenderingHelper#render` to push a new `nice_partial`
+  # on the stack, so rendering has a fresh `partial` to store content in.
   def render(*)
     __partials.prepend nice_partial
     super
@@ -53,6 +55,12 @@ module NicePartials::RenderingWithAutoContext
     __partials.shift
   end
 
+  # Action View passes any `yield`s in partials through `_layout_for`, our
+  # override detects if it's a capturing yield and appends the current partial
+  # to the arguments.
+  #
+  # So `render … do |some_object|` can become `render … do |some_object, partial|`
+  # without needing to find and update the inner `yield some_object` call.
   def _layout_for(*arguments, &block)
     if block && !arguments.first.is_a?(Symbol)
       capture_with_outer_partial_access(*arguments, &block)
@@ -79,6 +87,9 @@ end
 module NicePartials::PartialRendering
   ActionView::PartialRenderer.prepend self
 
+  # Automatically captures the `block` in case the partial has no manual capturing `yield` call.
+  #
+  # Effectively the same as having `<% yield partial %>` as the first partial line.
   def render_partial_template(view, locals, template, layout, block)
     view.capture_with_outer_partial_access(&block) if block && !template.has_capturing_yield?
     super
