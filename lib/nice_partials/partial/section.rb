cecb6044b99ccdb0f5b04a998f52ec5f25ada153
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-02 09:23:13 +0200

    Document proxying supports

--- lib/nice_partials/partial/section.rb
+++ lib/nice_partials/partial/section.rb
@@ -10,8 +10,21 @@ class NicePartials::Partial::Section < NicePartials::Partial::Content
 
   undef_method :p # Remove Kernel.p here to pipe through method_missing and hit tag proxy.
 
-  # Allows for doing `partial.body.render "form", tangible_thing: @tangible_thing`
-  # and `partial.body.link_to @document.name, @document`
+  # Implements our proxying to the `@view_context` or `@view_context.tag`.
+  #
+  # `@view_context` proxying forwards the message and automatically appends any content, so you can do things like:
+  #
+  #   <% partial.body.render "form", tangible_thing: @tangible_thing %>
+  #   <% partial.body.link_to @document.name, @document %>
+  #   <% partial.body.t ".body" %>
+  #
+  # `@view_context.tag` proxy lets you build bespoke elements based on content and options provided:
+  #
+  #    <% partial.title "Some title content", class: "xl" %> # Write the content and options to the `title`
+  #    <% partial.title.h2 ", appended" %> # => <h2 class="xl">Some title content, appended</h2>
+  #
+  # Note that NicePartials don't support deep merging attributes out of the box.
+  # For that, bundle https://github.com/seanpdoyle/attributes_and_token_lists
   def method_missing(meth, *arguments, **keywords, &block)
     if meth != :p && @view_context.respond_to?(meth)
       concat @view_context.public_send(meth, *arguments, **keywords, &block)
