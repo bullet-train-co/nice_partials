fc317c938f6464f8453cad641ae6145d8d088b68
Kasper Timm Hansen <kaspth@gmail.com>
2023-07-10 19:12:06 +0200

    Test out BRB template idea

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -27,3 +27,19 @@ class NicePartials::Test < ActionView::TestCase
     @page ||= Capybara.string(rendered)
   end
 end
+
+module BRB; end
+class BRB::Erubi < ::ActionView::Template::Handlers::ERB::Erubi
+  # DEFAULT_REGEXP = /<%(={1,2}|-|\#|%)?(.*?)([-=])?%>([ \t]*\r?\n)?/m
+  # DEFAULT_REGEXP = /<%(={1,2}|-|\#|%)?(.*?)([-=])?%>([ \t]*\r?\n)?/m
+
+  def initialize(input, ...)
+    old_input = input.dup
+    if input.gsub!(/\\(?!s\+)(.*?)(\<\/|[ \t]*\r?\n)/, '<%\1%>\2')
+      # puts [old_input, input] unless input.include?("clobbering")
+    end
+    super
+  end
+end
+
+ActionView::Template::Handlers::ERB.erb_implementation = BRB::Erubi
