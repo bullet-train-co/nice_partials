9f5eadccf586e9f1700cf670fc854c6fed17d2c9
Kasper Timm Hansen <kaspth@gmail.com>
2023-07-12 16:03:19 +0200

    Add a lorem sigil to help insert placeholder text and check UI handling long strings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -45,6 +45,9 @@ module BRB::Sigils
   register :attributes, '\= tag.attributes(\1)'
   register :p, '\= tag.attributes(\1)'
   register :data, '\= tag.attributes(data: \1)'
+  register :lorem, <<~end_of_lorem
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  end_of_lorem
 end
 
 class BRB::Erubi < ::ActionView::Template::Handlers::ERB::Erubi
